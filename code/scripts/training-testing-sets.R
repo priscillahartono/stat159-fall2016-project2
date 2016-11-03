98c0350f64db978fd059635418762a379d499978
Andrea Widjaja <andrea.widjaja@gmail.com>
2016-11-03 02:17:05 -0700

    uccode: updated training-testing-sets.R

--- code/scripts/training-testing-sets.R
+++ code/scripts/training-testing-sets.R
@@ -1,9 +1,17 @@
 scaled_credit <- read.csv('../../data/scaled-credit.csv')
 
-set.seed(10)
+set.seed(100)
 
 training <- sample(1:400, 300)
-testing <- (! training)
+testing <- (-training)
 
-training_set <- credit[training,]
-testing_set <- credit[testing,]
+training_set <- scaled_credit[training,]
+testing_set <- scaled_credit[testing,]
+
+save(training_set, testing_set, file='../../data/training-testing-sets.RData')
+save(training_set, file='../../data/training-set.csv')
+save(testing_set, file='../../data/testing-set.csv')
+
+
+
+
