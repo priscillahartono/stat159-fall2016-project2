0378ae83a025f51786088adcc9afb259b9bff694
Andrea Widjaja <andrea.widjaja@gmail.com>
2016-11-03 16:43:31 -0700

    code, data: modified ols, training, and scaled-credit

--- code/functions/ordinary-least-squares.R
+++ code/functions/ordinary-least-squares.R
@@ -4,11 +4,42 @@ train_data <- read.csv('../../data/training-set.csv')
 test_data <- read.csv('../../data/testing-set.csv')
 
 
-
-#Fit dataset
-ols <- lm(Balance~Income+Limit+Rating+Cards+Age+Education
-             +Gender+Student+Married+Ethnicity, 
-             data=as.data.frame(scaled_credit))
+#OLS is a method to estimate the unknown parameters in a linear regression model
+#with a goal of minimizing the sum of the squares of the differences between the observed responses and the predicted by a linear function of a set
 
 
-summary_of_ols <- summary(ols)
+#Examining fit of dataset
+#----------------------------------
+ols<-lm(Balance ~ . , 
+        data=scaled_credit)
+
+summary_ols <- summary(ols)
+
+#Mean Squared Error
+mse_ols <- mean(summary_ols$residuals^2)
+
+
+
+#Objects Saved in ols-regression.RData
+#----------------------------------
+
+save(ols, summary_ols, mse_ols, file = "../../data/ols-regression.RData")
+
+#Generate ols-regression.txt
+#----------------------------------
+sink("../../data/ols-regression-output.txt")
+
+cat("Ordinary Least Square\n")
+print(ols)
+
+cat("\n\n")
+
+cat("Summary Statistics of OLS\n")
+print(summary_ols)
+
+cat("\n\n")
+
+cat("Mean Squared Error (OLS)\n")
+print(mse_ols)
+sink()
+
--- code/scripts/training-testing-sets.R
+++ code/scripts/training-testing-sets.R
@@ -3,14 +3,12 @@ scaled_credit <- read.csv('../../data/scaled-credit.csv')
 set.seed(100)
 
 training <- sample(1:400, 300)
-testing <- (-training)
+testing <- (1:400)[-training]
 
 training_set <- scaled_credit[training,]
 testing_set <- scaled_credit[testing,]
 
 save(training_set, testing_set, file='../../data/training-testing-sets.RData')
-save(training_set, file='../../data/training-set.csv')
-save(testing_set, file='../../data/testing-set.csv')
 
 
 
